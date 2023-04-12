a356cbdbe48648492295c4ed05017c4e30fc635a
Alex <novikovprojects@gmail.com>
2023-04-12 19:52:12 +0300

    rename env vars for terraform

--- cloud-init.sh
+++ cloud-init.sh
@@ -2,6 +2,10 @@
 
 source cloud.conf
 
+export TF_VAR_PROJECT_ID=$PROJECT_ID
+export TF_VAR_USER_NAME=$USER_NAME
+export TF_VAR_PASSWORD=$PASSWORD
+
 terraform apply -auto-approve
 
-terraform output | head -n -1 | tail -n -1 > privkey.pem
+terraform output | head -n -1 | tail -n +2 > privkey.pem
